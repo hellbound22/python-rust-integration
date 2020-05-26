2a85032cbe1b058e8291daa1000e9d4e75fee716
Rodrigo <rodrigomauricio@protonmail.com>
2020-05-26 14:48:22 -0300

    Added openpyxl to the benchmark

--- python-test/test.py
+++ python-test/test.py
@@ -9,7 +9,7 @@ def rust_test():
 
     task.parse(s)
 
-def py_test():
+def oldpy_test():
     import json
     from xlrd import open_workbook
 
@@ -27,9 +27,24 @@ def py_test():
     json.dumps(values, ensure_ascii=False)
 
 
-if __name__ == "__main__":
-    setup = "from __main__ import rust_test, py_test"
+def py_test():
+    import json
+    from openpyxl import load_workbook
+    wb = load_workbook(filename='input.xlsx')["Sheet1"]
 
+    values = []
+
+    for r in wb.iter_rows():
+        item = {"sku": r[0].value, "designation": r[1].value, "price": r[2].value}
+        item["description"] = "%s (%s), $%s" % (item["designation"], item["sku"], item["price"])
+        values.append(item)
+
+    json.dumps(values, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    setup = "from __main__ import rust_test, py_test, oldpy_test"
     
     print("Rust implementation: " + str(ti.timeit("rust_test()", setup=setup, number=10000)))
-    print("Python native implementation: " + str(ti.timeit("py_test()", setup=setup, number=10000)))
+    print("openpyxl implementation: " + str(ti.timeit("py_test()", setup=setup, number=10000)))
+    print("Deceprecated xlrd implementation: " + str(ti.timeit("oldpy_test()", setup=setup, number=10000)))
