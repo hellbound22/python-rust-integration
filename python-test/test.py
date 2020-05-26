8c8aa2421fbd599f8424693670049e568f87ba52
Rodrigo <rodrigomauricio@protonmail.com>
2020-05-26 13:54:06 -0300

    Benchmarking tests

--- python-test/test.py
+++ python-test/test.py
@@ -1,8 +1,35 @@
-import task
+import timeit as ti
 
-f = open("input.xlsx", "rb")
-#f = open("test.py", "rb")
+def rust_test():
+    import task
 
-s = f.read()
+    f = open("input.xlsx", "rb")
 
-print(task.parse(s))
+    s = f.read()
+
+    task.parse(s)
+
+def py_test():
+    import json
+    from xlrd import open_workbook
+
+    wb = open_workbook('input.xlsx')
+
+    sh = wb.sheet_by_index(0)
+
+    values = []
+
+    for r in sh.get_rows():
+        item = {"sku": r[0].value, "designation": r[1].value, "price": r[2].value}
+        item["description"] = "%s (%s), $%s" % (item["designation"], item["sku"], item["price"])
+        values.append(item)
+
+    json.dumps(values, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    setup = "from __main__ import rust_test, py_test"
+
+    
+    print("Rust implementation: " + str(ti.timeit("rust_test()", setup=setup, number=10000)))
+    print("Python native implementation: " + str(ti.timeit("py_test()", setup=setup, number=10000)))
